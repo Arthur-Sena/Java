033eb412d3229010a84849361fcc5fa5594db071
Arthur-Sena <asassena1002@gmail.com>
2024-06-11 20:06:54 -0300

    Aula 2 - Completa (Orientada a objeto)

---
+++ Aula-2/src/Principal.java
@@ -0,0 +1,22 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+    }
+}
